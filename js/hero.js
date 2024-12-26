be3d728926169a1044173ea34e3e357d727149ca
mohammadalmasri27 <mohammadalmasrifreelance@gmail.com>
2024-12-26 00:06:42 -0800

    first v1 template future code

---
+++ js/hero.js
@@ -0,0 +1,6 @@
+try {
+    // document.querySelector("#video_background").style.width = `${document.querySelector("#hero").offsetWidth}px `;
+    // document.querySelector("#video_background").style.height = `${document.querySelector("#hero").offsetHeight}px `;
+} catch (error) {
+    
+}
